b6e8ce1381b83195bc0dd2ede30613d55cee4d6b
nel34 <nel.fodil@viacesi.fr>
2025-04-04 14:48:29 +0200

    resolved conflicts

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -4,10 +4,6 @@ import './main.sass'
 import Home from './pages/Home'
 import Connection from './pages/Connection'
 import Restaurant from './pages/Restaurant'
-import AccueilRestaurateur from './pages/AccueilRestaurateur'
-import PaiementsRestaurateur from './pages/PaiementsRestaurateur';
-import CommandesRestaurateur from './pages/CommandesRestaurateur';
-import MenuRestaurateur from './pages/MenuRestaurateur';
 
 createRoot(document.getElementById('root')).render(
   <Router>
@@ -20,10 +16,6 @@ createRoot(document.getElementById('root')).render(
       <Route path="/payment" />
       <Route path="/account" />
       <Route path="/account/orders" />
-      <Route path="/restaurateur" element={<AccueilRestaurateur />} />
-      <Route path="/restaurateur/paiements" element={<PaiementsRestaurateur />} />
-      <Route path="/restaurateur/commandes" element={<CommandesRestaurateur />} />
-      <Route path="/restaurateur/menu" element={<MenuRestaurateur />} />
     </Routes>
   </Router>,
 )
