6cc393fe44137d28ccb45d138d82d4ca3ab63fc4
EnzoMrcli <marcelli.enzo@gmail.com>
2025-04-07 21:47:51 +0200

    🔧 Update order status display in DeliveryOrderDetailsPopup for better clarity

--- front/src/components/DeliveryOrderDetailsPopup/index.jsx
+++ front/src/components/DeliveryOrderDetailsPopup/index.jsx
@@ -11,7 +11,12 @@ const OrderDetailsPopup = ({ order, onClose }) => {
         <p><strong>Commande ID :</strong> {order.order_id}</p>
         <p><strong>User ID :</strong> {order.user_id}</p>
         <p><strong>Restaurant ID :</strong> {order.restaurant_id}</p>
-        <p><strong>Status :</strong> {order.status}</p>
+        <p><strong>Status :</strong> {
+          order.status === "Pending_Delivery" ? "En attente de livraison" :
+          order.status === "Delivered" ? "Livrée" :
+          order.status === "Cancelled" ? "Annulée" :
+          order.status === "In_Progress" ? "En cours" : "Inconnu"
+        }</p>
         <p><strong>Prix :</strong> {order.menu_price}</p>
         <p><strong>Adresse :</strong> {order.location}</p>
         <button className="btn" onClick={onClose}>Fermer</button>
