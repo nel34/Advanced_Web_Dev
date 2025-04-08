b5837666d18e714403309c7847f16d0b9629f72f
Fabien Arrighi <fabien2a.arrighi@protonmail.com>
2025-04-08 14:06:27 +0200

    🔧 Refactor order and restaurant models: remove required fields for order_id and owner_id; update Checkout component to handle order creation with user data and delivery address input.

--- front/src/pages/Checkout/index.jsx
+++ front/src/pages/Checkout/index.jsx
@@ -2,22 +2,46 @@ import './index.sass'
 import { useCart } from '../../context/CartContext'
 import CartItem from '../../components/CartItem'
 import Button from '../../components/Button'
+import { useAuth } from '../../context/AuthContext'
+import { useFetchWithAuth } from '../../utils/hooks'
 
 export default function Checkout() {
   const { cart, getTotalPrice } = useCart()
   const deliveryPrice = 5
+  const { user } = useAuth()
+
+  const { isLoading, data, error } = useFetchWithAuth('GET', `http://localhost:8080/api/auth/users/${user.id}`)
+
+  const handleCheckout = async () => {
+    const order = {
+      restaurant_id: cart[0].restaurantId,
+      user_id: user.id,
+      username: data.username,
+      menu: cart.map((item) => item._id),
+      total: getTotalPrice() + deliveryPrice,
+      location: document.getElementById('address').value
+    }
+
+    const response = await fetch('http://localhost:8080/api/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`
+      },
+      body: JSON.stringify(order)
+    })
+  }
 
   return (
     <div className='home home--secondary'>
       <div className='info-section'>
         <h2>Détails de la livraison</h2>
-        <div></div>
+        <div className='form__input'>
+          <label htmlFor='address'>Address de livraison</label>
+          <input id='address' type='text' placeholder='Address de livraison' />
+        </div>
         <div className='line'></div>
-        <div></div>
-        <div className='line'></div>
-        <div></div>
-        <div className='line'></div>
-        <Button content={'Valider la commande'} type='primary' onClick={() => alert('Commande validée !')} />
+        <Button content={'Valider la commande'} type='primary' onClick={handleCheckout} />
       </div>
       <div className='info-section-layout--2'>
         <div className='info-section'>
@@ -42,7 +66,7 @@ export default function Checkout() {
               <p>Total</p>
               <p>{getTotalPrice() + deliveryPrice} €</p>
             </div>
-            <Button content={'Valider la commande'} type='primary' onClick={() => alert('Commande validée !')} />
+            <Button content={'Valider la commande'} type='primary' onClick={handleCheckout} />
           </div>
         </div>
       </div>
